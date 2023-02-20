8552561efbfa49b0365b0eaa76fd26b9b8dc7aa3
Britaney Fleming <britaneyrfleming@gmail.com>
2023-02-20 10:53:28 -0600

    added .env

---
+++ prana-yoga-app/server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const { PORT } = process.env;
+const { sequelize } = require("./util/database");
+const { register, login } = require("./controllers/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", register);
+app.post("/login", login);
+
+sequelize
+    .sync({ force: true })
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(
+                `Connection successful. Server running on port ${PORT}`
+            )
+        );
+    })
+    .catch((err) => console.log(err));
